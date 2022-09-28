343faa30465a7822334d5567aa762a556b12589a
Rakin Rahman <rakinhacker@gmail.com>
2022-09-28 22:38:15 +0600

    Add utils.h and its implementation

---
+++ include/utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+namespace utils
+{
+    // Trim functions
+    std::string ltrim(const std::string& s);
+    std::string rtrim(const std::string& s);
+    std::string trim(const std::string& s);
+
+} // namespace utils
+
+#endif
