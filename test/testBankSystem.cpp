e974f8ec3f8ff4cea3232fe5f954f4e39b721d77
Rakin Rahman <rakinhacker@gmail.com>
2022-09-28 18:57:41 +0600

    Add test for bankSystem.h

---
+++ test/testBankSystem.cpp
@@ -0,0 +1,9 @@
+#include "../include/bankSystem.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Bank System test function is 2 (pass)", "[single-file]")
+{
+    BankSystem system;
+    REQUIRE(system.testFunc() == 2);
+}
