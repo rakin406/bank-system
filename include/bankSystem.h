80ca19bf5514d80e26793fbaef2999790e841208
Rakin Rahman <rakinhacker@gmail.com>
2022-09-28 18:51:31 +0600

    Add bankSystem.h header file

---
+++ include/bankSystem.h
@@ -0,0 +1,25 @@
+#ifndef BANK_SYSTEM_H
+#define BANK_SYSTEM_H
+
+/**
+ * @brief This class contains the main functionalities of Bank.
+ */
+class BankSystem
+{
+public:
+    /**
+     * @brief Default constructor for Bank System initialization.
+     */
+    // BankSystem();
+
+    /**
+     * @brief This is a test method for unit testing.
+     *
+     * @return integer.
+     */
+    static inline int testFunc() { return 1 + 1; }
+
+private:
+};
+
+#endif
