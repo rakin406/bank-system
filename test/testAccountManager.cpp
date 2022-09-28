c44da721ceb9b7a6fc7f7801bb9be1ca25c4cf8c
Rakin Rahman <rakinhacker@gmail.com>
2022-09-28 20:57:48 +0600

    Pass test for isEligible() method

--- test/testAccountManager.cpp
+++ test/testAccountManager.cpp
@@ -1,3 +1,11 @@
 #include "../include/accountManager.h"
 
 #include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Account Manager isEligible() method returns false on initialization "
+          "(pass)",
+          "[multi-file:1]")
+{
+    AccountManager manager;
+    REQUIRE(manager.isEligible() == false);
+}
