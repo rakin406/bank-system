918e517bd00068d569959e88d17018d6ec6b5307
Rakin Rahman <rakinhacker@gmail.com>
2022-09-28 20:56:49 +0600

    Add accountManager.cpp implementation

---
+++ src/accountManager.cpp
@@ -0,0 +1,14 @@
+#include "../include/accountManager.h"
+
+#include <algorithm>
+
+namespace
+{
+    // unsigned int getAgeFromBirthDate();
+} // namespace
+
+bool AccountManager::isEligible() const { return m_eligible; }
+
+// bool AccountManager::isBirthDateValid() {}
+
+// bool AccountManager::isPhoneNumberValid() {}
