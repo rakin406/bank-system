26fa6cd0e3dc26af922545315f528a197d1a4949
Rakin Rahman <rakinhacker@gmail.com>
2022-09-28 22:38:32 +0600

    Test utils.h

---
+++ test/testUtils.cpp
@@ -0,0 +1,10 @@
+#include "../include/utils.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("\"   Hello World   \" is equal to \"Hello World\""
+          "(pass)",
+          "[multi-file:1]")
+{
+    REQUIRE(utils::trim("   Hello World   ") == "Hello World");
+}
