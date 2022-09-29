69a426b41f77d3b656321f2fc0398e9e13fb55ed
Rakin Rahman <rakinhacker@gmail.com>
2022-09-29 07:15:12 +0600

    Pass account equals comparison test

--- test/testAccountManager.cpp
+++ test/testAccountManager.cpp
@@ -2,6 +2,15 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+TEST_CASE("Account struct \"==\" comparison returns true"
+          "(pass)",
+          "[multi-file:3]")
+{
+    Account account{ "Darth Vader", "", "" };
+    Account duplicate{ account };
+    REQUIRE(duplicate == account);
+}
+
 TEST_CASE("AccountManager::isEligible() method returns false on initialization "
           "(pass)",
           "[multi-file:3]")
