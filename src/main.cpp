3f5580bce925e1d276edb6a622fe66899b8df4ef
Rakin Rahman <rakinhacker@gmail.com>
2022-09-30 13:57:29 +0600

    Get inputs and handle wrong inputs

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,69 @@
 #include "../include/account.h"
 
 #include <iostream>
+#include <string>
+#include <string_view>
+
+namespace setup
+{
+    /**
+     * @brief Get input for account information until it's valid.
+     *
+     * @param account Client account.
+     */
+    void getCorrectInput(Account* account);
+} // namespace setup
 
 int main()
 {
     Bank bank;
-    Account account{ "Rakin", 20, 3000 };
+
+    std::string name{};
+    unsigned int age{};
+    int wallet{};
+
+    std::cout << "Enter name: ";
+    std::getline(std::cin >> std::ws, name);
+    std::cout << "Enter age: ";
+    std::cin >> age;
+    std::cout << "Enter wallet cash: ";
+    std::cin >> wallet;
+
+    Account account{ static_cast<std::string_view>(name), age, wallet };
+
+    if (!account.isValid())
+    {
+        setup::getCorrectInput(&account);
+    }
+
+    std::cout << "Account is valid!\n";
 
     return 0;
+}
+
+void setup::getCorrectInput(Account* account)
+{
+    std::string name{};
+    unsigned int age{};
+
+    // Keep getting input until account is valid
+    while (!account->isValid())
+    {
+        if (!account->isNameValid())
+        {
+            std::cout << "Invalid name\n";
+            std::cout << "Enter name again: ";
+            std::getline(std::cin >> std::ws, name);
+        }
+        else if (!account->isAgeValid())
+        {
+            std::cout << "Underage\n";
+            std::cout << "Enter age again: ";
+            std::cin >> age;
+        }
+
+        // Reset values
+        account->name = name;
+        account->age = age;
+    }
 }
